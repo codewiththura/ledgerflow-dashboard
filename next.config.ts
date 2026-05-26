4d7b8f50a0a07271975ea31004895adf56180b53
Thura <thura.00011@gmail.com>
2026-05-26 17:24:31 +0630

    chore: comment out experimental cpus and allowed dev origins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    cpus: 2,
-  },
+  // experimental: {
+  //   cpus: 2,
+  // },
+  // allowedDevOrigins: ['172.20.96.1'],
 };
 
 export default nextConfig;
